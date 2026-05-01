63a6603df8f919325d07a4f19d82b946a2d48560
LeonardoBarreraS <leonardobs86@gmail.com>
2026-04-30 19:33:04 -0500

    cambio

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import sys
 import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+# sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
 
 from shell.api.app import create_app  # noqa: E402
 
 app = create_app()
+
+# if __name__ == "__main__":
+#     import uvicorn
+
+#     uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=False)
+
